ef16dffb37a5b15e408e49a095ec4b2352802de4
Ghuysu <daogiahuysu@gmail.com>
2024-12-24 01:43:06 +0700

    Update service page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC<HeaderProps> = ({ nickname, path, setPath }) => {
 
   return (
     <div className="relative">
-      <header className="flex justify-between items-center lg:px-16 md:px-6 px-6 py-4 relative bg-gradient-to-b from-gray-100 to-gray-50">
+      <header className="flex justify-between items-center lg:px-16 md:px-6 px-6 py-4 relative bg-gradient-to-b from-zinc-100 to-zinc-50">
         <div>
           <p className="text-3xl font-bold text-main_red">
             {nickname}
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -2,19 +2,19 @@ import { ServicesProps } from '../interface';
 
 const Services: React.FC<ServicesProps> = ({ services }) => {
   return (
-    <div className="bg-white text-black h-[calc(100vh-theme(height.20))] lg:mx-16 xl:mx-16 md:mx-16 sm:mx-16 mx-6 grid grid-cols-1 place-content-start md:place-content-center">
+    <div className="bg-white text-black h-[calc(100vh-theme(height.20))] mx-6 sm:mx-16 md:mx-30 xl:mx-60 grid grid-cols-1 place-content-start md:place-content-center">
       <div className="grid grid-cols-1 mt-5 md:mt-0 md:grid-cols-2 gap-12 md:gap-24 lg:gap-32">
         {services.map((service, index) => (
           <div
             key={index}
             className="flex flex-col gap-y-3 md:gap-y-5 lg:gap-y-8 border-t-0 pb-4 border-l-0 border-r-0 border-b-[1px] border-solid border-black relative group"
-          >
-            <p className="text-base md:text-xl lg:text-2xl font-extrabold group-hover:text-main_red">{`0${index + 1}.`}</p>
-            <p className="text-2xl md:text-3xl lg:text-4xl font-bold group-hover:text-main_red">
+          > 
+            <p className="text-base font-extrabold group-hover:text-main_red">{`0${index + 1}.`}</p>
+            <p className="text-xl font-bold group-hover:text-main_red">
               {service.name}
             </p>
-            <p className="text-justify">{service.description}</p>
-            <div className="absolute right-0 flex items-center justify-center text-white text-xl rounded-xl p-1 w-8 h-8 md:text-2xl md:rounded-2xl md:p-2 md:w-10 md:h-10 lg:text-3xl lg:rounded-2xl lg:p-2 lg:w-14 lg:h-14 bg-black">
+            <p className="text-justify text-xs">{service.description}</p>
+            <div className="absolute right-0 flex items-center justify-center text-white text-xl rounded-xl p-1 w-8 h-8 bg-black">
               {index + 1 === 1 && (
                 <i className="a-duotone fa-solid fa-server"></i>
               )}
